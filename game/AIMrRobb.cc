0b173fc5ca05e711ba6f5bb19eca119854452245
Mr.Robb <roberto.ariosa@est.fib.upc.edu>
2017-11-30 12:51:56 +0100

    MrRobb_v1

--- game/AIMrRobb.cc
+++ game/AIMrRobb.cc
@@ -5,7 +5,7 @@
  * Write the name of your player and save this file
  * with the same name and .cc extension.
  */
-#define PLAYER_NAME MrRobb
+#define PLAYER_NAME MrRobb_v1
 
 
 struct PLAYER_NAME : public Player {
