88380673707a4ee20577eca60c811a941aa88062
Mr.Robb <roberto.ariosa@est.fib.upc.edu>
2017-12-05 12:33:56 +0100

    constant seeds

--- game/go2.sh
+++ game/go2.sh
@@ -1,13 +1,14 @@
-make
+make > /dev/null
 
 PLAYER="MrRobb_v3"
-ENEMY="Dummy"
+ENEMY="MrRobb_v2"
 
-echo "Submission 14"
+echo "\n$PLAYER vs $ENEMY\n"
 
-echo "1 with seed 761188" && ./Game $PLAYER $PLAYER $PLAYER $PLAYER --seed 761188 < default.cnf > default.out 2> cerr.out && grep "score" cerr.out && echo "\n"
-echo "2 with seed 463092" && ./Game $PLAYER $ENEMY $ENEMY $ENEMY --seed 463092 < default.cnf > default.out 2> cerr.out && grep "score" cerr.out && echo "\n"
-echo "3 with seed 209390" && ./Game $PLAYER $ENEMY $ENEMY $ENEMY --seed 209390 < default.cnf > default.out 2> cerr.out && grep "score" cerr.out && echo "\n"
-echo "4 with seed 905326" && ./Game $PLAYER $ENEMY $ENEMY $ENEMY --seed 905326 < default.cnf > default.out 2> cerr.out && grep "score" cerr.out && echo "\n"
-echo "5 with seed 709595" && ./Game $PLAYER $ENEMY $ENEMY $ENEMY --seed 709595 < default.cnf > default.out 2> cerr.out && grep "score" cerr.out && echo "\n"
-echo "6 with seed 916538" && ./Game $PLAYER $ENEMY $ENEMY $ENEMY --seed 709595 < default.cnf > default.out 2> cerr.out && grep "score" cerr.out && echo "\n"
+echo "200 rounds";
+
+for (( i = 0; i < 100; i++ )); do
+    echo "$i with seed $i" && ./Game $PLAYER $ENEMY $ENEMY $ENEMY --seed $i < default.cnf > default.out 2> cerr.out && grep "$ENEMY got top score" cerr.out;
+done
+
+rm -f cerr.out
