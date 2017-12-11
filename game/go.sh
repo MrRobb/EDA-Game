2810460f5de39c028df86224bf02cec80dec10e7
Mr.Robb <roberto.ariosa@est.fib.upc.edu>
2017-12-11 16:29:38 +0100

    scripts

--- game/go.sh
+++ game/go.sh
@@ -1,7 +1,7 @@
 make > /dev/null
 
-PLAYER="MrRobb_v3"
-ENEMY="MrRobb_v2"
+PLAYER="MrRobb_v4"
+ENEMY="mcasellas"
 
 echo "\n$PLAYER vs $ENEMY\n"
 
@@ -9,21 +9,21 @@ echo "200 rounds";
 
 for (( i = 0; i < 100; i++ )); do
     RNUM=$(od -An -N4 -tu2 /dev/urandom | awk '{print $1}')
-    echo "$i with seed $RNUM" && ./Game $PLAYER MrRobb_v2 mcasellas MrRobb_v2 --seed $RNUM < default.cnf > default.out 2> cerr.out && grep "got top score" cerr.out;
+    echo "$i with seed $RNUM" && ./Game $PLAYER mcasellas Null Null --seed $RNUM < default.cnf > default.out 2> cerr.out && grep -e  "mcasellas" -e "MrRobb_v4" cerr.out | grep -e "score";
 done
 
 echo "400 rounds";
 
 for (( i = 0; i < 100; i++ )); do
     RNUM=$(od -An -N4 -tu2 /dev/urandom | awk '{print $1}')
-    echo "$i with seed $RNUM" && ./Game $PLAYER MrRobb_v2 mcasellas MrRobb_v2 --seed $RNUM < default400.cnf > default.out 2> cerr.out && grep "got top score" cerr.out;
+    echo "$i with seed $RNUM" && ./Game $PLAYER mcasellas Null Null --seed $RNUM < default400.cnf > default.out 2> cerr.out && grep "got top score" cerr.out;
 done
 
 echo "1000 rounds";
 
 for (( i = 0; i < 100; i++ )); do
     RNUM=$(od -An -N4 -tu2 /dev/urandom | awk '{print $1}')
-    echo "$i with seed $RNUM" && ./Game $PLAYER MrRobb_v2 mcasellas MrRobb_v2 --seed $RNUM < default1000.cnf > default.out 2> cerr.out && grep "got top score" cerr.out;
+    echo "$i with seed $RNUM" && ./Game $PLAYER mcasellas Null Null --seed $RNUM < default1000.cnf > default.out 2> cerr.out && grep "got top score" cerr.out;
 done
 
 rm -f cerr.out
--- game/go2.sh
+++ game/go2.sh
@@ -1,6 +1,6 @@
 make > /dev/null
 
-PLAYER="MrRobb_v3"
+PLAYER="MrRobb_v4"
 ENEMY="mcasellas"
 
 echo "\n$PLAYER vs $ENEMY\n"
@@ -8,7 +8,7 @@ echo "\n$PLAYER vs $ENEMY\n"
 echo "200 rounds";
 
 for (( i = 0; i < 100; i++ )); do
-    echo "$i with seed $i" && ./Game $PLAYER $ENEMY MrRobb_v2 Dummy --seed $i < default.cnf > default.out 2> cerr.out && grep "$PLAYER got top score" cerr.out;
+    echo "$i with seed $i" && ./Game $PLAYER $ENEMY Null Null --seed $i < default.cnf > default.out 2> cerr.out && grep "$ENEMY got top score" cerr.out;
 done
 
 rm -f cerr.out
