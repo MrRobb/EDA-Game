a896137b525d9cbeb829cd1bfe5b20cc6f3f0e8f
Mr.Robb <roberto.ariosa@est.fib.upc.edu>
2017-12-15 12:19:10 +0100

    sh

--- game/go2.sh
+++ game/go2.sh
@@ -1,4 +1,4 @@
-PLAYER="MrRobb_v6"
+PLAYER="MrRobb_v7"
 ENEMY="MrRobb_v5"
 
 echo "\n$PLAYER vs $ENEMY\n"
@@ -6,7 +6,11 @@ echo "\n$PLAYER vs $ENEMY\n"
 echo "200 rounds";
 
 for (( i = 0; i < 100; i++ )); do
-    echo "$i with seed $i" && ./Game $PLAYER $ENEMY $ENEMY $ENEMY --seed $i < default.cnf > default.out 2> cerr.out && grep "$ENEMY got top score" cerr.out;
+	echo "$i with seed $i";
+	./Game $PLAYER $ENEMY $ENEMY $ENEMY --seed $i < default.cnf > default.out 2> cerr.out && grep "top score" cerr.out;
+	./Game $ENEMY $PLAYER $ENEMY $ENEMY --seed $i < default.cnf > default.out 2> cerr.out && grep "top score" cerr.out;
+	./Game $ENEMY $ENEMY $PLAYER $ENEMY --seed $i < default.cnf > default.out 2> cerr.out && grep "top score" cerr.out;
+	./Game $ENEMY $ENEMY $ENEMY $PLAYER --seed $i < default.cnf > default.out 2> cerr.out && grep "top score" cerr.out;
 done
 
 rm -f cerr.out
