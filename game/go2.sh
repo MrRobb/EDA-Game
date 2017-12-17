5f38dc595ac2edc33470e4d286279551d559ccc3
Mr.Robb <roberto.ariosa@est.fib.upc.edu>
2017-12-17 16:29:42 +0100

    go

--- game/go2.sh
+++ game/go2.sh
@@ -9,10 +9,10 @@ echo "200 rounds";
 
 for (( i = 0; i < 100; i++ )); do
 	echo "$i with seed $i";
-	./Game $PLAYER $ENEMY $ENEMY $ENEMY --seed $i < default.cnf > default.out 2> cerr.out && grep "top score" cerr.out;
-	./Game $ENEMY $PLAYER $ENEMY $ENEMY --seed $i < default.cnf > default.out 2> cerr.out && grep "top score" cerr.out;
-	./Game $ENEMY $ENEMY $PLAYER $ENEMY --seed $i < default.cnf > default.out 2> cerr.out && grep "top score" cerr.out;
-	./Game $ENEMY $ENEMY $ENEMY $PLAYER --seed $i < default.cnf > default.out 2> cerr.out && grep "top score" cerr.out;
+	./Game $PLAYER $ENEMY $ENEMY $ENEMY --seed $i < default.cnf > default2.out 2> cerr2.out && grep "top score" cerr2.out;
+	./Game $ENEMY $PLAYER $ENEMY $ENEMY --seed $i < default.cnf > default2.out 2> cerr2.out && grep "top score" cerr2.out;
+	./Game $ENEMY $ENEMY $PLAYER $ENEMY --seed $i < default.cnf > default2.out 2> cerr2.out && grep "top score" cerr2.out;
+	./Game $ENEMY $ENEMY $ENEMY $PLAYER --seed $i < default.cnf > default2.out 2> cerr2.out && grep "top score" cerr2.out;
 done
 
-rm -f cerr.out
+rm -f cerr2.out
